dbf658b14526e9ac7df54d291e3df9cc5a4a979c
Vinay Patel <vinay.light@hotmail.co.uk>
2020-10-08 12:32:04 +0100

    initial commit

---
+++ Ex_Calculator A4/CalculatorLib/BracketObjectBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLib
+{
+
+    public class BracketObjectBuilder
+    {
+        public List<string> ListofBracketObjects { get; set; } //To contain numbers and opperators
+
+        public BracketObjectBuilder(string inputEquation)
+        {
+            int inputEquationLength = inputEquation.Length;
+            for (int item1 = 0; item1 < inputEquationLength; item1++)
+            {
+                if (inputEquation[item1] == '(' || inputEquation[item1] == ')')
+                {
+                    int BracketPartOpen = item1;
+                    for (int item2 = BracketPartOpen + 1; item2 < inputEquationLength; item2++)
+                    {
+                        if (inputEquation[item2] == '(')
+                        {
+                            string Part = inputEquation.Substring(item1, item2 - item1);
+                            ListofBracketObjects.Add(Part);
+                            item1 = item2;
+                        }
+                        if (inputEquation[item2] == ')')
+                        {
+                            string Part = inputEquation.Substring(item1, item2 - item1 + 1);
+                            ListofBracketObjects.Add(Part);
+                            item1 = item2 + 1;
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+
+        public List<string> ReturnListofBracketObjects()
+        {
+            return ListofBracketObjects;
+        }
+
+
+
+    }
+}
---
+++ Ex_Calculator A4/CalculatorLib/BracketObjectSimplifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLib
+{
+    
+    public class BracketObjectSimplifier
+    {
+        public List<String> ListofBracketParts { get; set; } //To contain numbers and opperators
+
+        public BracketObjectSimplifier(string equation)
+        {
+            string numBuilder = "";
+            foreach (char i in equation)
+                if (Char.IsDigit(i) == true || i == '.')
+                {
+                    numBuilder += i;
+                }
+                else if (i == '-' || i == '+' || i == '/' || i == '*')
+                {
+                    if (numBuilder != "")
+                    {
+                        ListofBracketParts.Add(numBuilder);                        
+                    }
+                    else if (i == '-') 
+                    {
+                        numBuilder += i;
+                    }
+                    else
+                    {
+                        numBuilder = "";
+                        ListofBracketParts.Add(i.ToString());
+                    }                   
+                    
+                }
+        }
+    }
+}
